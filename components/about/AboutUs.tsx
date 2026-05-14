50e463299972f54c34879aeb393a099753af15fb
jose <joseernestoroldan@gmail.com>
2026-05-14 00:32:09 -0400

    feat: create About Us page and component with mission overview and image section

--- components/about/AboutUs.tsx
+++ components/about/AboutUs.tsx
@@ -1,24 +1,22 @@
-"use client";
-import React from "react";
-import Heading from "../Heading/heading";
-import HeadingAboutAnimation from "../headingsAnimations/HeadingAboutAnimation";
+import Container from "../layouts/container/Container";
+import SmokeText from "../SmokeText/SmokeText";
 import Image from "next/image";
 
 const AboutUs = () => {
   return (
-    <HeadingAboutAnimation>
-      <div className="flex flex-col-reverse justify-end md:flex-row md:items-center md:justify-center w-full h-[90vh]">
-        <div className="w-full md:w-[50%] flex flex-col justify-between md:justify-center items-center space-y-10">
-          <div className="w-full relative text-wrap pt-8 md:pt-0">
-            <Heading
-              title="About Magic Marble Foundation"
-              color="text-cyan-500"
-              shadow=""
-              textSize="text-3xl md:text-5xl"
-              center="text-center lg:text-left"
+    <Container>
+      <div className="flex flex-col-reverse md:flex-row items-center justify-center w-full min-h-[calc(100vh-140px)] gap-12 py-16">
+        
+        {/* Text Section */}
+        <div className="w-full md:w-1/2 flex flex-col justify-center space-y-10">
+          <div className="w-full relative text-wrap">
+            <SmokeText
+              text="About Magic Marble Foundation"
+              className="text-3xl md:text-4xl lg:text-5xl font-bold text-cyan-600 drop-shadow-sm md:justify-start"
+              staggerMs={40}
             />
           </div>
-          <div className=" w-full">
+          <div className="w-full">
             <p className="w-full indent-6 text-base md:text-lg lg:text-xl text-gray-500 font-medium text-justify ">
               Magic Marble Foundation works to alleviate the suffering of
               underprivileged human and non-human animal populations by
@@ -28,20 +26,22 @@ const AboutUs = () => {
             </p>
           </div>
         </div>
-        <div className="w-full md:w-[50%] flex flex-col justify-center items-center">
-          <div className="w-[220px] md:w-[330px] h-[220px] md:h-[330px] relative">
-            {/* photo container */}
+
+        {/* Image Section */}
+        <div className="w-full md:w-1/2 flex flex-col justify-center items-center">
+          <div className="w-[280px] h-[280px] md:w-[350px] md:h-[350px] relative rounded-full overflow-hidden shadow-[0_0_40px_rgba(6,182,212,0.15)] ring-4 ring-cyan-500/20">
             <Image
-              className="object-cover object-top"
+              className="object-cover object-top hover:scale-105 transition-transform duration-700 ease-in-out"
               src="/logo.jpg"
               fill
-              alt="Magic Marbel Foundation"
+              alt="Magic Marble Foundation"
               priority={true}
             />
           </div>
         </div>
+
       </div>
-    </HeadingAboutAnimation>
+    </Container>
   );
 };
 
